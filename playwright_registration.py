0b2402d1f3c9057ad4cdf9a56fc1a317bc0fdd64
SpRiTkun <sprit797@gmail.com>
2025-09-16 22:12:05 +0300

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -2,8 +2,9 @@ from playwright.sync_api import sync_playwright, expect
 
 
 with sync_playwright() as playwright:
-    chromium = playwright.chromium.launch(headless=False)
-    page = chromium.new_page()
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()  # Создание контекста
+    page = context.new_page() # Создание страницы
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
@@ -23,5 +24,15 @@ with sync_playwright() as playwright:
     expect(dashboard_toolbar).to_be_visible()
     expect(dashboard_toolbar).to_have_text('Dashboard')
 
+    page.wait_for_timeout(5000)
+
+    context.storage_state(path="browser-state.json")
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json") # Указываем файл с сохраненным состоянием
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
 
     page.wait_for_timeout(5000)
